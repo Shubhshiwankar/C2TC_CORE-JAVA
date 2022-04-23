4b1e22688977692a97c45c1339978a1f49077478
admin <admin@DESKTOP-I5J0ADS>
2022-04-23 16:26:39 +0530

    Array

---
+++ DAY15/src/one_dimensional_array/DemoArray.java
@@ -0,0 +1,19 @@
+package one_dimensional_array;
+
+public class DemoArray {
+	public static void main (String args[])
+	{
+		//initialize array
+		int a[]=new int[5];
+		a[0]=34;
+		a[1]=12;
+		a[2]=3;
+		a[3]=78;
+		a[4]=2;
+		for(int i=0; i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
