0fe4f416d8d1398ef3d92c5a8a29f21c0de78887
admin <admin@DESKTOP-I5J0ADS>
2022-04-23 16:14:08 +0530

    car

--- DAY14/src/PACK14/Demo_Wrap.java
+++ DAY14/src/PACK14/Demo_Wrap.java
@@ -3,13 +3,15 @@ package PACK14;
 public class Demo_Wrap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int i=20;
-Integer a=i;
-Integer j=new Integer(10);
-System.out.println(+a);
-System.out.println(+j);
-System.out.println(+i);
+		
+		int i=30;
+		Integer a=i;
+		@SuppressWarnings("removal")
+		Integer j=new Integer(15);
+		System.out.println(a+" "+i+" "+j);
+		//System.out.println(+a);
+		//System.out.println(+j);	
+		//System.out.println(+i);
 	}
 
 }
---
+++ DAY4/src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+	//member variable
+	//member function,methods
+	int noofcars;
+	String Model;
+	
+	void display()
+	{
+		System.out.println("Welcome to benz ");
+	}
+
+	
+
+		public static void main(String[] args) {
+		
+		Car obj = new Car();//obj creation
+		obj.display();
+		
+		
+	}
+
+}
