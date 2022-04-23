f740d601372a7dbeddb89c175fd58ca3f0e49e33
admin <admin@DESKTOP-I5J0ADS>
2022-04-23 16:37:03 +0530

    java collection

---
+++ DAY21/src/javacollection/hashmapdemo.java
@@ -0,0 +1,15 @@
+package javacollection;
+
+import java.util.HashMap;
+
+public class hashmapdemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> hmap=new HashMap<Integer,String>();
+		hmap.put(1, "tns");
+		hmap.put(2, "cap");
+		System.out.println(hmap);
+
+	}
+
+}
